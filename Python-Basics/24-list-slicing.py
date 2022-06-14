f99cb036480f98465ef20d850cf1b6cbfff13e06
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-13 22:42:25 -0700

    slicing lists

---
+++ Python-Basics/24-list-slicing.py
@@ -0,0 +1,6 @@
+list = [11,33,44,66,788,1]
+print(list[0:5])  # this will return list starting from index 0 to index 4
+
+print(list[:3]) # this will return list starting from index  0 to index 3 too
+
+print(list[2:])
