7cf7e6c114c4e3f1dadf5cd32e03f1ba53b793dc
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-14 21:49:19 -0700

    the output seems similar but the data types are different

---
+++ Python-Basics/44-number-to-string.py
@@ -0,0 +1,4 @@
+i = 1075
+print("number (i) = ", i)
+print("string (i) = ", str(i))  # if string contains non numeric character
+                             # then int() will throw ValueError exception.
