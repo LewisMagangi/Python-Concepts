8ffae84f21a86217543634f14d60ccf7d9d09a91
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-14 21:21:07 -0700

    creating a turple

---
+++ Python-Basics/40-turple-create.py
@@ -0,0 +1,11 @@
+b1 = ()  # creates an empty tuple with no data
+
+b2 = (11,22,33)
+
+b3 = tuple([1,2,3,4,4])  # tuple from array
+
+b4 = tuple("abc")  # tuple from string
+print("b1 =", b1)
+print("b2 =", b2)
+print("b3 =", b3)
+print("b4 =", b4)
