9ea4101642c845abfa3dcda5a715cee840cbf62e
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-16 03:37:11 -0700

    trying out some basic math functions

---
+++ Python-Basics/65-math-func.py
@@ -0,0 +1,20 @@
+import math
+
+t = (13, 56, 9, 13, 34, 42)
+print("The absolute value of -22 is ", abs(-22))  # Returns the absolute value
+
+print("The min no in the turple t is", max(t))  # Returns the maximum number
+
+print("The min no in turple t is ", min(t))  # Returns the minimum number
+
+print("8 power 2 is", pow(8, 2))  # can also be written as 8 ** 2
+
+print("4.1 power 3.1 is", pow(4.1, 3.2))  # can also be written as 4.1 ** 3.2
+
+print("5.32 rounded off to the nearest int is", round(5.32))  # Rounds to its nearest integer
+
+print("3.1456875712 rouded off to 3 decimal places is", round(3.1456875712, 3)) # Return number with 3 digits after decimal point
+
+print("Rounding 3.4123 up is", math.ceil(3.4123))
+
+print("Rounding 24.99231 down is", math.floor(24.99231))
