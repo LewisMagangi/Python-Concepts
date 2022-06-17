7b28da1a7cfa51a487f26c8866347674fe4e3e7a
Liquelaliqour <lewismomanyi34@gmail.com>
2022-06-17 13:26:53 -0700

    code fix

--- Python-Basics/75-operator-overloading.py
+++ Python-Basics/75-operator-overloading.py
@@ -17,11 +17,11 @@ class Circle:
     def __add__(self, another_circle):
         return Circle( self.__radius + another_circle.__radius )
 
-    c1 = Circle(4)
-    print(c1.getRadius())
+c1 = Circle(4)
+print(c1.getRadius())
 
-    c2 = Circle(5)
-    print(c2.getRadius())
+c2 = Circle(5)
+print(c2.getRadius())
 
-    c3 = c1 + c2 # This became possible because we have overloaded + operator by adding a    method named __add__
-    print(c3.getRadius())
+c3 = c1 + c2 # This became possible because we have overloaded + operator by adding a    method named __add__
+print(c3.getRadius())
