6ec2a304b1ffcfd18e0b5302d6b5d1455a7a0423
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-08 13:53:47 +0000

    wuuueh hii nmetii

--- board.py
+++ board.py
@@ -1,3 +1,18 @@
-for i in range(25):
-    for j in range(13):
-        print("board[" +str(i) + "][" + str(j) + "]")
+board = []
+null = " "
+value = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+for col in range(13):
+    row = [null for i in range(25)]
+    board.append(row)
+
+for col in range(13):
+    if row[0] or row[8] or row[16] or row[24]:
+        if col == 0 or col == 4 or col == 8 or col == 12:
+            board.insert( , "+")
+    elif col == 0 or col == 4 or col == 8 or col == 12:
+        if row != 0 and row != 8 and row != 16 and row != 24:
+            board.insert(row, "-") 
+
+#print(board[row][l])
+print(board)
