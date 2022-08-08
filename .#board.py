9766de8499981b15cd27448c214f90b9fa16ed0e
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-08 16:41:36 +0000

    still stuck

---
+++ .#board.py
@@ -0,0 +1,1 @@
+root@dcc1fb0a3d46.6401
--- board.py
+++ board.py
@@ -1,18 +1,18 @@
-board = []
-null = " "
-value = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+row, col = (13, 25)
+board = [[0]*col]*row
 
-for col in range(13):
-    row = [null for i in range(25)]
-    board.append(row)
-
-for col in range(13):
-    if row[0] or row[8] or row[16] or row[24]:
-        if col == 0 or col == 4 or col == 8 or col == 12:
-            board.insert( , "+")
-    elif col == 0 or col == 4 or col == 8 or col == 12:
-        if row != 0 and row != 8 and row != 16 and row != 24:
-            board.insert(row, "-") 
+for i in row:
+    for j in col:
+        if j == 0 or j == 8 or j == 16 or j == 24:
+            if i == 0 or i == 4 or i == 8 or i == 12:
+                board[i][j] = "+"
+            else:
+                board[i][j] = "|"
+        elif i != 0 or i != 4 or i != 8 or i != 12:
+            if j != 0 and row != 8 and row != 16 and row != 24:
+                board.insert(row, "-") 
 
 #print(board[row][l])
+print(board)
+print(row, col)
 print(board)
