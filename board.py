595f6d6bf3dc462312dbb06be269d89171ac8fbf
Liquelaliqour <lewismomanyi34@gmail.com>
2022-08-08 19:10:46 +0000

    a brand new start

--- board.py
+++ board.py
@@ -1,21 +1,36 @@
-row, col = (13, 25)
-board = [[0]*col]*row
-
-for i in range(row):
-    for j in range(col):
-        if j == 0 or j == 8 or j == 16 or j == 24:
-            if i == 0 or i == 4 or i == 8 or i == 12:
-                board[i][j] = "+"
-            elif i != 0 or i != 4 or i != 8 or 1 != 12:
-                board[i][j] = "|"
-        elif j != 0 or j != 8 or j != 16 or j != 24:
-            if i == 0 or i == 4 or i == 8 or i == 12:
-                board[i][j] = "-"
-        """elif i != 0 or i != 4 or i != 8 or i != 12:
-            if j != 0 and row != 8 and row != 16 and row != 24:
-                board.insert(row, "-") 
-
-#print(board[row][l])
-print(board)"""
-print(row, col)
-print(board)
+board = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+] 
+def first_line (board):
+    print("+-------+-------+-------+")
+def second_line (board):
+    print("|       |       |       |")
+def blank_line (board):
+    print("                         ")
+first_line(board)
+blank_line(board)
+second_line(board)
+blank_line(board)
+print("|   " + str(board[0][0]) + "   |   " + str(board[0][1]) + "   |   " + str(board[0][2]) + "   |")
+blank_line(board)
+second_line(board)
+blank_line(board)
+first_line(board)
+blank_line(board)
+second_line(board)
+blank_line(board)
+print("|   " + str(board[1][0]) + "   |   " + str(board[1][1]) + "   |   " + str(board[1][2]) + "   |")
+blank_line(board)
+second_line(board)
+blank_line(board)
+first_line(board)
+blank_line(board)
+second_line(board)
+blank_line(board)
+print("|   " + str(board[2][0]) + "   |   " + str(board[2][1]) + "   |   " + str(board[2][2]) + "   |")
+blank_line(board)
+second_line(board)
+blank_line(board)
+first_line(board)
