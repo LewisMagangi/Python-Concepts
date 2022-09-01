81d25da18cbf20444fd33a5a694e203b97b73399
Liquelaliqour <lewismomanyi34@gmail.com>
2022-09-01 14:56:31 +0000

    using the pass keyword

---
+++ The-Python-Tutorial-Python-Docs/Control-Flows/class-pass.py
@@ -0,0 +1,5 @@
+'''
+This is commonly used for creating minimal classes:
+'''
+class MyEmptyClass:
+     pass
---
+++ The-Python-Tutorial-Python-Docs/Control-Flows/placeholder-pass.py
@@ -0,0 +1,5 @@
+'''
+Another place pass can be used is as a place-holder for a function or conditional body when you are working on new code, allowing you to keep thinking at a more abstract level. The pass is silently ignored:
+'''
+def initlog(*args):
+     pass   # Remember to implement this!
