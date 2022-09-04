916cc7d44723c66f16d123cf960530fb57f11264
Liquelaliqour <lewismomanyi34@gmail.com>
2022-09-04 12:25:01 +0000

    more code

---
+++ The-Python-Tutorial-Python-Docs/Control-Flows/Loops/enumerate2.py
@@ -0,0 +1,5 @@
+"""
+When looping through a sequence, the position index and corresponding value can be retrieved at the same time using the enumerate() function.
+"""
+for i, v in enumerate(['tic', 'tac', 'toe', 'is', 'an', 'amazing', 'game']):
+     print(i, v)
---
+++ The-Python-Tutorial-Python-Docs/Control-Flows/Loops/items.py
@@ -0,0 +1,7 @@
+"""
+When looping through dictionaries, the key and corresponding value can be retrieved at the same time using the items() method.
+"""
+
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+for k, v in knights.items():
+    print(k, v)
