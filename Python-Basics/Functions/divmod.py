4c88b374f4673a59adf3f07c729dd112a24138f2
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-06 05:00:52 -0800

    divmod function implemetation

---
+++ Python-Basics/Functions/divmod.py
@@ -0,0 +1,10 @@
+"""
+The divmod() function returns a tuple containing the quotient  and the remainder when argument1 (dividend) is divided by argument2 (divisor).
+
+syntax:
+
+divmod(dividend, divisor)
+"""
+x = divmod(113, 3)
+
+print(x)
