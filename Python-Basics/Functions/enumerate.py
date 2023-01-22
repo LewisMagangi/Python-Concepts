65e0fb4baacd3782ed4cf1418f14e5c3e0feb67a
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-22 06:16:58 -0800

    enumerate function

---
+++ Python-Basics/Functions/enumerate.py
@@ -0,0 +1,14 @@
+# Python program to illustrate
+# enumerate function
+list1 = ["eat", "sleep", "code", "repeat"]
+string1 = "geekforgeeks"
+
+# creating enumerate objects
+obj1 = enumerate(list1)
+obj2 = enumerate(string1)
+
+print ("Return type:", type(obj1))
+print (list(enumerate(list1)))
+
+# changing start index to 5 from 0
+print (list(enumerate(string1, 5)))
