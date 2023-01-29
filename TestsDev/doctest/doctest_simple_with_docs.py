d90e02ed0a4fc7c3cdf16b550c45fd5765c4464b
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-29 09:42:57 -0800

    commenting texts

--- TestsDev/doctest/doctest_simple_with_docs.py
+++ TestsDev/doctest/doctest_simple_with_docs.py
@@ -1,3 +1,9 @@
+"""
+doctest also allows for surrounding text.
+It looks for lines beginning with the interpreter prompt (>>>) to find the beginning of a test case,
+and the case is ended by a blank line or by the next interpreter prompt. 
+Intervening text is ignored, and can have any format as long as it does not look like a test case.
+"""
 def my_function(a, b):
     """
     Returns a * b.
